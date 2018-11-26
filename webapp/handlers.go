824f22b88c431af89e788846354230198d4625dd
Calin Pristavu <calinpristavu@gmail.com>
2018-11-26 14:20:17 +0200

    Isolate handlers and main package code

--- webapp/handlers.go
+++ webapp/handlers.go
@@ -2,121 +2,13 @@ package webapp
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
 )
-
-type handler struct {
-	db         *gorm.DB
-	templating struct {
-		choiceQuestion      *template.Template
-		textQuestion        *template.Template
-		flowDiagramQuestion *template.Template
-		finished            *template.Template
-		quizHistory         *template.Template
-		login               *template.Template
-		home                *template.Template
-		myAccount           *template.Template
-	}
-}
-
-var h *handler
-
-func Init(db *gorm.DB, r *mux.Router) {
-	h = &handler{
-		db: db,
-	}
-
-	r.HandleFunc("/login", page)
-
-	sr := r.NewRoute().Subrouter()
-	sr.Use(AuthMiddleware)
-
-	sr.HandleFunc("/question", question)
-	sr.HandleFunc("/finished", finished)
-	sr.HandleFunc("/quiz-history", history)
-	sr.HandleFunc("/quiz-history/{id}", viewQuiz)
-	sr.HandleFunc("/start", startQuiz)
-
-	sr.HandleFunc("/", home)
-	sr.HandleFunc("/me", myAccount)
-	sr.HandleFunc("/logout", logout)
-
-	h.db.AutoMigrate(
-		&User{},
-		&Quiz{},
-		&Question{},
-		&ChoiceAnswer{},
-		&TextAnswer{},
-		&FlowDiagramAnswer{},
-		&QuizTemplate{},
-		&QuestionTemplate{},
-		&ChoiceAnswerTemplate{},
-	)
-
-	var err error
-	h.templating.choiceQuestion, err = template.ParseFiles("templates/choice_question.gohtml", "header.gohtml", "footer.gohtml")
-	if err != nil {
-		log.Fatalf("could not parse template: %v", err)
-	}
-
-	h.templating.textQuestion, err = template.ParseFiles("templates/text_question.gohtml", "header.gohtml", "footer.gohtml")
-	if err != nil {
-		log.Fatalf("could not parse template: %v", err)
-	}
-
-	h.templating.login, err = template.ParseFiles("templates/login.gohtml")
-	if err != nil {
-		log.Fatalf("could not parse template: %v", err)
-	}
-
-	h.templating.home, err = template.ParseFiles("templates/home.gohtml", "header.gohtml", "footer.gohtml")
-	if err != nil {
-		log.Fatalf("could not parse template: %v", err)
-	}
-
-	h.templating.myAccount, err = template.ParseFiles("templates/myAccount.gohtml", "header.gohtml", "footer.gohtml", "account_nav.gohtml")
-	if err != nil {
-		log.Fatalf("could not parse template: %v", err)
-	}
-
-	h.templating.flowDiagramQuestion, err = template.ParseFiles(
-		"templates/flow_diagram_question.gohtml",
-		"templates/flow_diagram_js.gohtml",
-		"header.gohtml",
-		"footer.gohtml",
-	)
-	if err != nil {
-		log.Fatalf("could not parse template: %v", err)
-	}
-
-	h.templating.finished, err = template.ParseFiles(
-		"templates/finished.gohtml",
-		"templates/flow_diagram_js.gohtml",
-		"header.gohtml",
-		"footer.gohtml",
-	)
-	if err != nil {
-		log.Fatalf("could not parse template: %v", err)
-	}
-
-	h.templating.quizHistory, err = template.ParseFiles(
-		"templates/history.gohtml",
-		"templates/flow_diagram_js.gohtml",
-		"header.gohtml",
-		"footer.gohtml",
-		"account_nav.gohtml",
-	)
-	if err != nil {
-		log.Fatalf("could not parse template: %v", err)
-	}
-}
 
 func startQuiz(w http.ResponseWriter, r *http.Request) {
 	u := r.Context().Value("user").(*User)
