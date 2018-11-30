08e081b0ee6b3a34b0c9daa41d8a779a31b84b74
Calin Pristavu <calinpristavu@gmail.com>
2018-11-30 13:06:48 +0200

    Create and edit question forms without answers

--- admin/src/views/Quiz/QuestionTemplates/QuestionTemplates.js
+++ admin/src/views/Quiz/QuestionTemplates/QuestionTemplates.js
@@ -11,99 +11,191 @@ import {
   Input
 } from 'reactstrap'
 
+const views = {
+  create: 1,
+  edit: 2,
+  view: 3
+};
+
 class QuestionTemplates extends Component{
-  questions = [
-    {
-      Text: "Quesiton 1",
-      Type: 1,
-      AnswerTemplates: ["Checkbox 1"]
-    }
-  ];
+  state = {
+    openedView: views.create,
+    editItem: {},
+    questions: [
+      {
+        ID: 1,
+        Text: "Quesiton 1",
+        Type: 1,
+        AnswerTemplates: ["Checkbox 1"]
+      }
+    ]
+  };
 
-  renderCreateNew() {
+  openCreateView = () => {
+    this.setState({
+      openedView: views.create
+    })
+  };
 
-  }
+  openEditView = (item) => {
+    this.setState({
+      openedView: views.edit,
+      editItem: item
+    })
+  };
 
   render() {
     return (
       <div className="animated fadeIn">
         <Row>
-          {/*List card*/}
           <Col xs="12" lg="6">
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify" /> Question Templates
-                <span className="float-right">
-                  <i
-                    onClick={this.renderCreateNew}
-                    className="fa fa-plus-circle text-success"
-                    style={{cursor: "pointer"}}/>
-                </span>
-              </CardHeader>
-              <CardBody>
-                <Table responsive>
-                  <thead>
-                    <tr>
-                      <th>Text</th>
-                      <th>Type</th>
-                      <th>Answer Template</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {this.questions.map((q, k) =>
-                      <tr key={k}>
-                        <td>{q.Text}</td>
-                        <td>{q.Type}</td>
-                        <td></td>
-                      </tr>
-                    )}
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
+            <QuestionsList
+              questions={this.state.questions}
+              openCreateView={this.openCreateView}
+              openEditView={this.openEditView}/>
           </Col>
 
-          {/*Create card*/}
           <Col xs="12" lg="6">
-            <Card>
-              <CardHeader>
-                <i className="fa fa-plus-circle text-success" />
-                <strong>Create Question</strong>
-                <small> Form</small>
-              </CardHeader>
-              <CardBody>
-                <Row>
-                  <Col xs="12">
-                    <FormGroup>
-                      <Label htmlFor="question-text">Text</Label>
-                      <Input type="text" id="question-text" placeholder="Type in the question text" required />
-                    </FormGroup>
-                  </Col>
-                </Row>
-                <FormGroup row>
-                  <Col md="12">
-                    <Label>Type</Label>
-                  </Col>
-                  <Col md="12">
-                    <FormGroup check inline>
-                      <Input className="form-check-input" type="radio" id="question-type-1" name="type" value="1" />
-                      <Label className="form-check-label" check htmlFor="question-type-1">One</Label>
-                    </FormGroup>
-                    <FormGroup check inline>
-                      <Input className="form-check-input" type="radio" id="question-type-2" name="type" value="2" />
-                      <Label className="form-check-label" check htmlFor="question-type-2">Two</Label>
-                    </FormGroup>
-                    <FormGroup check inline>
-                      <Input className="form-check-input" type="radio" id="question-type-3" name="type" value="3" />
-                      <Label className="form-check-label" check htmlFor="question-type-3">Three</Label>
-                    </FormGroup>
-                  </Col>
-                </FormGroup>
-              </CardBody>
-            </Card>
+            {this.state.openedView === views.create ?
+              <CreateQuestion/> : null
+            }
+            {this.state.openedView === views.edit ?
+              <EditQuestion question={this.state.editItem}/> : null
+            }
           </Col>
         </Row>
       </div>
+    );
+  }
+}
+
+class QuestionsList extends Component {
+  render() {
+    return (
+
+      <Card>
+        <CardHeader>
+          <i className="fa fa-align-justify" /> Question Templates
+          <span className="float-right">
+                  <i
+                    onClick={this.props.openCreateView}
+                    className="fa fa-plus-circle text-success"
+                    style={{cursor: "pointer"}}/>
+                </span>
+        </CardHeader>
+        <CardBody>
+          <Table responsive>
+            <thead>
+            <tr>
+              <th>Text</th>
+              <th>Type</th>
+              <th>Answer Template</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.props.questions.map((q, k) =>
+              <tr key={k} onClick={() => {this.props.openEditView(q)}}>
+                <td>{q.Text}</td>
+                <td>{q.Type}</td>
+                <td></td>
+              </tr>
+            )}
+            </tbody>
+          </Table>
+        </CardBody>
+      </Card>
+    );
+  }
+}
+
+class CreateQuestion extends Component {
+  render() {
+    return (
+
+      <Card>
+        <CardHeader>
+          <i className="fa fa-plus-circle text-success" />
+          <strong>Create Question</strong>
+          <small> Form</small>
+        </CardHeader>
+        <CardBody>
+          <Row>
+            <Col xs="12">
+              <FormGroup>
+                <Label htmlFor="question-text">Text</Label>
+                <Input type="text" id="question-text" placeholder="Type in the question text" required />
+              </FormGroup>
+            </Col>
+          </Row>
+          <FormGroup row>
+            <Col md="12">
+              <Label>Type</Label>
+            </Col>
+            <Col md="12">
+              <FormGroup check inline>
+                <Input className="form-check-input" type="radio" id="question-type-1" name="type" value="1" />
+                <Label className="form-check-label" check htmlFor="question-type-1">Checkboxes</Label>
+              </FormGroup>
+              <FormGroup check inline>
+                <Input className="form-check-input" type="radio" id="question-type-2" name="type" value="2" />
+                <Label className="form-check-label" check htmlFor="question-type-2">Free text</Label>
+              </FormGroup>
+              <FormGroup check inline>
+                <Input className="form-check-input" type="radio" id="question-type-3" name="type" value="3" />
+                <Label className="form-check-label" check htmlFor="question-type-3">Flow Diagram</Label>
+              </FormGroup>
+            </Col>
+          </FormGroup>
+        </CardBody>
+      </Card>
+    );
+  }
+}
+
+class EditQuestion extends Component {
+  render() {
+    return (
+      <Card>
+        <CardHeader>
+          <i className="fa fa-edit text-warning" />
+          <strong>Editing question {this.props.question.ID}</strong>
+          <small> Form</small>
+        </CardHeader>
+        <CardBody>
+          <Row>
+            <Col xs="12">
+              <FormGroup>
+                <Label htmlFor="question-text">Text</Label>
+                <Input
+                  type="text"
+                  id="question-text"
+                  placeholder="Type in the question text"
+                  required
+                  defaultValue={this.props.question.Text}/>
+              </FormGroup>
+            </Col>
+          </Row>
+          <FormGroup row>
+            <Col md="12">
+              <Label>Type</Label>
+            </Col>
+            <Col md="12">
+              <FormGroup check inline>
+                <Input className="form-check-input" defaultChecked={this.props.question.Type === 1} type="radio" id="question-type-1" name="type" value="1" />
+                <Label className="form-check-label" check htmlFor="question-type-1">Checkboxes</Label>
+              </FormGroup>
+              <FormGroup check inline>
+                <Input className="form-check-input" defaultChecked={this.props.question.Type === 2} type="radio" id="question-type-2" name="type" value="2" />
+                <Label className="form-check-label" check htmlFor="question-type-2">Free text</Label>
+              </FormGroup>
+              <FormGroup check inline>
+                <Input className="form-check-input" defaultChecked={this.props.question.Type === 3} type="radio" id="question-type-3" name="type" value="3" />
+                <Label className="form-check-label" check htmlFor="question-type-3">Flow Diagram</Label>
+              </FormGroup>
+            </Col>
+          </FormGroup>
+        </CardBody>
+      </Card>
     );
   }
 }
