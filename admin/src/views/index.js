2edc7afef36b9c375a7ccccac1a74e638a7e5de3
Calin Pristavu <calinpristavu@gmail.com>
2019-01-14 15:17:45 +0200

    Remove unused widgets

--- admin/src/views/index.js
+++ admin/src/views/index.js
@@ -19,13 +19,10 @@ import {
 } from './Base';
 
 import { ButtonDropdowns, ButtonGroups, Buttons, BrandButtons } from './Buttons';
-import Charts from './Charts';
-import Dashboard from './Dashboard';
 import { CoreUIIcons, Flags, FontAwesome, SimpleLineIcons } from './Icons';
 import { Alerts, Badges, Modals } from './Notifications';
 import { Login, Page404, Page500, Register } from './Pages';
 import { Colors, Typography } from './Theme';
-import Widgets from './Widgets';
 
 export {
   Badges,
@@ -48,9 +45,6 @@ export {
   Tooltips,
   Tabs,
   Tables,
-  Charts,
-  Dashboard,
-  Widgets,
   Jumbotrons,
   Switches,
   ProgressBar,
