8c23c8d36302144f4e7e8db970febb205eaa43f6
Calin Pristavu <calinpristavu@gmail.com>
2019-01-16 10:51:13 +0200

    My OCD is now contempt

--- admin/src/containers/DefaultLayout/DefaultFooter.js
+++ admin/src/containers/DefaultLayout/DefaultFooter.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const propTypes = {
-  children: PropTypes.node,
-};
-
-const defaultProps = {};
-
 class DefaultFooter extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+  };
+
+  static defaultProps = {};
+
   render() {
     return (
       <React.Fragment>
-        <span><a href="https://evozon.com">Quiz App</a> &copy; evozon.</span>
+        <span><a href="https://quiz.evozon.com">Quiz App</a> &copy; <a href="https://evozon.com">evozon.</a></span>
         <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
       </React.Fragment>
     );
   }
 }
-
-DefaultFooter.propTypes = propTypes;
-DefaultFooter.defaultProps = defaultProps;
 
 export default DefaultFooter;
